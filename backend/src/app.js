e4ae859c7a5ee4b7eb78db05ede77e62599c1cbf
Ashish Sen <ashishsen2609@gmail.com>
2025-05-03 12:50:14 +0530

    cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,6 +11,10 @@ app.use(cors({
     credentials: true,
    
 }))
+app.options("*", cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+  }));
 
 app.use(express.json({limit: "16kb"}))                              /*data kisi bhi form m ara ho ,charo alag alag handle karenge */
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
@@ -18,10 +22,7 @@ app.use(express.static("public"))
 app.use(cookieParser())
 console.log(process.env.CORS_ORIGIN)
 
-app.options("*", cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
-  }));
+
 
 //routes import
 import userRouter from './routes/user.routes.js'
