075181aa1ad7a35d00a4a9a46eceaf943b36a1ab
Ashish Sen <ashishsen2609@gmail.com>
2025-05-03 17:24:33 +0530

    offo

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -8,9 +8,7 @@ import { authenticateUser } from "../midllewears/authentication.middlewear.js"
 const router=Router()
 
 router.route("/register").post(upload.single("avatar") ,registerUser)
-router.route("/login").post(loginUser , (req, res) => {
-    console.log("Login route hit");
-  })
+router.route("/login").post(loginUser)
 router.route("/update").post(updateAccountDetails)
 router.route("/logout").post(authenticateUser , logoutUser)
 
